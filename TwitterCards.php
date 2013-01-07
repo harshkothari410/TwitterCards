803a978ff7a1b402f2648277fa6af9dcb8b80e3c
harshkothari410 <harshkothari410@gmail.com>
2013-01-07 23:41:23 +0530

    added file

---
+++ TwitterCards.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * TwitterCards
+ *
+ * 
+ * Extensions
+ * @author Harsh Kothari (http://mediawiki.org/wiki/User:Harsh4101991) <harshkothari410@gmail.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * 
+ */
+if ( !defined( 'MEDIAWIKI' ) ) die( "This is an extension to the MediaWiki package and cannot be run standalone." );
+
+$wgExtensionCredits['parserhook'][] = array (
+	"path" => __FILE__,
+	"name" => "TwitterCards",
+	"author" => "[http://mediawiki.org/wiki/User:Harsh4101991]",
+	'descriptionmsg' => '',
+	'url' => 'http://bugzilla.wikimedia.org/show_bug.cgi?id=43436',
+);
